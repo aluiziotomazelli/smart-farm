ae49d6e9dbfd15d5a68b0d25766ef92eaa3d7f47
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-04-23 23:44:38 -0400

    fix: add ultrasonic_sensor dependency and update idf_component.yml

--- app_test/main/main.cpp
+++ app_test/main/main.cpp
@@ -1,6 +1,9 @@
 #include "wifi_manager.hpp"
 #include "float_switch.hpp"
 #include "power_control.hpp"
+#include "us_sensor.hpp"
+#include <cstdint>
+#include "soc/gpio_num.h"
 
 extern "C" void app_main(void)
 {
@@ -26,4 +29,20 @@ extern "C" void app_main(void)
     power_control::GpioHAL gpio_hal;
     power_control::PowerControl power_control(gpio_hal, GPIO_NUM_4);
     power_control.init();
+
+    // Instantiate Ultrasonic Sensor
+    const gpio_num_t TRIG_PIN = GPIO_NUM_21;
+    const gpio_num_t ECHO_PIN = GPIO_NUM_7;
+
+    ultrasonic::UsConfig us_cfg = {
+        .ping_interval_ms = 70,
+        .ping_duration_us = 20,
+        .timeout_us = 25000,
+        .filter = ultrasonic::Filter::DOMINANT_CLUSTER,
+        .min_distance_cm = 25.0f,
+        .max_distance_cm = 200.0f,
+        .warmup_time_ms = 0};
+
+    ultrasonic::UsSensor us_sensor(TRIG_PIN, ECHO_PIN, us_cfg);
+    us_sensor.init();
 }
